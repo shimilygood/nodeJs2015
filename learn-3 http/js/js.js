0f64e9c03ca7d1d4069c50e314b90d9fea40ea80
shimily <11233@qq.com>
2015-12-24 10:40:04 +0800

    Z

--- learn-3 http/js/js.js
+++ learn-3 http/js/js.js
@@ -1,40 +1,8 @@
-/*上下文，及作用域*/
+var http = require('http');
 
-var a = 92;
-var b = "sdfx";
-
-function fn(){
-	console.log(a);  //92
-	console.log(b);  //undefined,此处b指fn函数作用域里的b,但是执行到此处，还未定义
-	var b = "sss";
-}
-fn();
-
-/**************************************/
-function Country(){
-	this.name = name;
-}
-
-function City(){
-	this.name = name;
-}
-
-function ClassRoom(){
-	this.name = name;
-}
-
-var country = new Country("中国");
-
-function Factory(){
-	var city = new City("北京");
-
-}
-
-Factory();
-
-
-
-
+http.createServer(function(req,res){
+	res.end('hello');
+}).listen(8080);
 
 
 
